37195855fad4112493f64b906aa0df74e5fde80d
MattiaToscano <mattia1997ct@gmail.com>
2025-05-15 13:42:08 +0200

    Importato e create nuove pagine

--- react-router/src/App.jsx
+++ react-router/src/App.jsx
@@ -1,4 +1,18 @@
-import { useState } from 'react'
+import { react } from 'react'
+
+//Importo Axios
+import axios from 'axios'
+
+//Importo useState, useEffect
+import { useState, useEffect } from 'react'
+
+//Importo Bootstrap
+import "bootstrap/dist/css/bootstrap.min.css"
+
+//Importo Routers
+import { BrowserRouter, Routes, Route } from 'react-router-dom'
+
+
 
 
 function App() {
