d7c280197c0b08ff7026940af9c66eb78f06d9e7
MattiaToscano <mattia1997ct@gmail.com>
2025-05-16 13:29:44 +0200

    [UPD] Aggiunta logica per recuperare e visualizzare i dettagli del prodotto

--- react-router/src/pages/product/DetailProduct.jsx
+++ react-router/src/pages/product/DetailProduct.jsx
@@ -3,6 +3,7 @@ import axios from 'axios'
 import { useParams, useNavigate } from 'react-router-dom'
 import { useState, useEffect } from 'react'
 import "bootstrap/dist/css/bootstrap.min.css"
+import { Button } from 'bootstrap'
 
 const DetailProduct = () => {
     const params = useParams();
@@ -11,5 +12,43 @@ const DetailProduct = () => {
 
     const [product, setProduct] = useState({})
 
+    const getSingleProduct = () => {
+        axios.get(`https://fakestoreapi.com/products/${id}`).then((resp) => {
+            setProduct(resp.data)
+        });
+    }
 
+    useEffect(() => {
+        getSingleProduct();
+    }, []);
+
+    return (
+        <div className="product-page">
+            <div className="header-container">
+                <h1 className="page-title">Products</h1>
+            </div>
+
+            <div className="products-container">
+                <img src={product.image} alt="" className='img-fluid' />
+            </div>
+            <h1>
+                {product.title}
+            </h1>
+            <span>
+                {product.title}
+            </span>
+            <span>
+                {product.description}
+            </span>
+            <button
+                onClick={() =>
+                    navigate(`./product/${id + 1}`)
+                }
+            >
+                Next Product
+            </button>
+        </div>
+    );
 }
+
+export default DetailProduct;
